f1afa38b1ebbbc218a90b68cc10ff90664b2b5d2
Saurabh <devsaurabh086@gmail.com>
2025-04-15 23:56:40 +0530

    React-Stage1-useStateHook-stage-1

---
+++ React-Serious/src/App.jsx
@@ -0,0 +1,18 @@
+
+
+export default function App() {
+
+  
+  const ingredients = ["Apple" , "Guava" , "Orange"];
+
+
+  return (
+    <>
+     
+      <main>
+        <h1>useState</h1>
+      </main>
+
+    </>
+  )
+}
